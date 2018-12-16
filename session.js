94a29de0006e7507de84207361f92a12ac4b6362
Brent Rinchiuso <brentrin@gmail.com>
2018-12-15 22:09:47 -0600

    More tabs or death.

--- session.js
+++ session.js
@@ -1,39 +1,38 @@
 var Player  = require('./player.js');
 
-module.exports = class User {
+module.exports = class Session {
 
-  constructor(socket) {
-    this._socket = socket;
-  }
+	constructor(socket) {
+		this._socket = socket;
+	}
 
-  sessionOpened() {
-    console.log('Session was opened.');
+	sessionOpened() {
+		console.log('Session was opened.');
 
-    this._socket.on('enter', this.processPlayerEntry);
-    this._socket.on('chat', this.processChatMessage);
-    this._socket.on('disconnect', this.sessionClosed);
-  }
+		this._socket.on('enter', this.processPlayerEntry);
+		this._socket.on('chat', this.processChatMessage);
+		this._socket.on('disconnect', this.sessionClosed);
+	}
 
-  sessionClosed() {
-    console.log('Session was closed.');
-    // do other stuff
-  }
+	sessionClosed() {
+		console.log('Session was closed.');
+		// do other stuff
+	}
 
-  processPlayerEntry(message) {
-    this._player = new Player(message.name);
+	processPlayerEntry(message) {
+		this._player = new Player(message.name);
+		// console.log(this._player.name.concat(' has joined!'));
+	}
 
-    // console.log(this._player.name.concat(' has joined!'));
-  }
-
-  processChatMessage(message) {
-    // console.log(message);
-    // if (typeof this._player == "undefined") {
-    //   console.log('Uninitialzied player tried sending a message!');
-    //   return;
-    // }
-    if (message.sender === this._player.name)
-      this._socket.broadcast.emit('chat', message); // not sure if this is right
-    else
-      console.log('User '.concat(this._player.name, ' tried to send a message for someone else!'));
-  }
+	processChatMessage(message) {
+		// console.log(message);
+		// if (typeof this._player == "undefined") {
+		//   console.log('Uninitialzied player tried sending a message!');
+		//   return;
+		// }
+		if (message.sender === this._player.name)
+			this._socket.broadcast.emit('chat', message); // not sure if this is right
+		else
+			console.log('User '.concat(this._player.name, ' tried to send a message for someone else!'));
+	}
 }
